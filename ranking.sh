51a9145ec2206b38ea06ef715ffee2b48d566893
Hayden Barnes <hayden.barnes@hpe.com>
2024-02-13 01:03:23 -0500

    Run devops topic search

--- ranking.sh
+++ ranking.sh
@@ -29,10 +29,10 @@ for i in {1..10}; do
     # repos_page=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $YOUR_GITHUB_TOKEN" "https://api.github.com/search/repositories?q=topic:linux&sort=stars&per_page=100&page=$i" | jq -r '.items[].clone_url')
 
     # Get projects tagged with 'wsl' topic
-    repos_page=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $YOUR_GITHUB_TOKEN" "https://api.github.com/search/repositories?q=topic:wsl&sort=stars&per_page=100&page=$i" | jq -r '.items[].clone_url')
+    # repos_page=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $YOUR_GITHUB_TOKEN" "https://api.github.com/search/repositories?q=topic:wsl&sort=stars&per_page=100&page=$i" | jq -r '.items[].clone_url')
 
     # Get projects tagged with 'devops' topic
-    # repos_page=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $YOUR_GITHUB_TOKEN" "https://api.github.com/search/repositories?q=topic:devops&sort=stars&per_page=100&page=$i" | jq -r '.items[].clone_url')
+    repos_page=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $YOUR_GITHUB_TOKEN" "https://api.github.com/search/repositories?q=topic:devops&sort=stars&per_page=100&page=$i" | jq -r '.items[].clone_url')
 
     # Get projects tagged with 'azure' topic
     # repos_page=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $YOUR_GITHUB_TOKEN" "https://api.github.com/search/repositories?q=topic:azure&sort=stars&per_page=100&page=$i" | jq -r '.items[].clone_url')
